349419618c692ef905d59322f6b28da6ec21056d
David Caballero Moreno <98742614+DavidCaballeroMoreno@users.noreply.github.com>
2022-06-08 09:30:05 +0200

    Lo he empezado,y casi terminado

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (input.magneticForce(Dimension.Strength)))
+})
 basic.forever(function () {
-	
+    if (input.magneticForce(Dimension.Strength) < 2100) {
+        music.playMelody("B - B - B - B - ", 120)
+        basic.showString("Intruso")
+    }
 })
